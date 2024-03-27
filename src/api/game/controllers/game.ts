e1845458311467a850ea8550af979de0a2495702
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-03-27 03:23:53 -0300

    feat: create custom route and controller

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -4,4 +4,13 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController('api::game.game', ({ strapi }) => ({
+  async populate(ctx) {
+    try {
+      console.log('Initializer')
+      ctx.body = 'ok'
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}));
---
+++ src/api/game/routes/custom-route.ts
@@ -0,0 +1,13 @@
+export default {
+  routes: [
+    { // Path defined with a URL parameter
+      method: 'POST',
+      path: '/games/populate',
+      handler: 'game.populate',
+      config: {
+        auth: false,
+        policies: []
+      }
+    },
+  ]
+}
