8946cdb5b91f7b15b3a283787834356ca959224a
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-03-27 17:26:43 -0300

    feat: add gameInfo

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,17 +1,36 @@
-/**
- * game service
- */
-
 import { factories } from '@strapi/strapi';
 import axios from 'axios'
+import jsdom from 'jsdom'
+import { slugify } from '../../../utils/slugify';
 
+import { GamesList } from '../../../types/gameTypes';
+
+async function getGameInfo(slug) {
+  const { JSDOM } = jsdom
+
+  const body = await axios.get(`https://www.gog.com/game/${slug}`)
+  const dom = new JSDOM(body.data)
+
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
 
 export default factories.createCoreService('api::game.game', ({ strapi }) => ({
   async populate(params) {
     const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
 
-    const { data: { products } } = await axios.get(gogApiUrl)
-
+    const { data: { products } } = await axios.get<GamesList>(gogApiUrl)
     console.log(products[0])
+    // console.log(await getGameInfo(products[0].slug))
+
+    await strapi.service('api::publisher.publisher').create({
+      name: products[0].publishers[0],
+      slug: slugify(products[0].publishers[0])
+    })
   }
 }));
