2ae75c70a75360866e57ce8b3a2188e81bed39b6
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-03-28 03:35:54 -0300

    feat: create many to many

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import jsdom from 'jsdom'
 import { slugify } from '../../../utils/slugify';
 
-import { GamesList } from '../../../types/gameTypes';
+import { GamesList, Product } from '../../../types/gameTypes';
 
 async function getGameInfo(slug: string) {
   const { JSDOM } = jsdom
@@ -42,13 +42,46 @@ async function create(name: string, entityName: string) {
   }
 }
 
+async function createManyToManyData(products: Array<Product>) {
+  const developers = {}
+  const publishers = {}
+  const categories = {}
+  const platforms = {}
+
+  products.forEach((product) => {
+    const { developers, publishers, genres, operatingSystems } = product
+
+    genres && genres.forEach((genre) => {
+      categories[genre.name] = true
+    })
+
+    operatingSystems && operatingSystems.forEach((os) => {
+      platforms[os] = true
+    })
+
+    developers && developers.forEach((developer) => {
+      developers[developer] = true
+    })
+
+    publishers && publishers.forEach((publisher) => {
+      publishers[publisher] = true
+    })
+  })
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, 'developer')),
+    ...Object.keys(publishers).map((name) => create(name, 'publisher')),
+    ...Object.keys(categories).map((name) => create(name, 'category')),
+    ...Object.keys(platforms).map((name) => create(name, 'platform')),
+  ])
+}
+
 export default factories.createCoreService('api::game.game', ({ strapi }) => ({
   async populate(params) {
     const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
 
     const { data: { products } } = await axios.get<GamesList>(gogApiUrl)
 
-    await create(products[1].publishers[0], 'publisher')
-    await create(products[1].developers[0], 'developer')
+    await createManyToManyData(products)
   }
 }));
