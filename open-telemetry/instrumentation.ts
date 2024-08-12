4d4665ec44ae55cd646dee8566946e61334564c1
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-12 07:38:15 -0300

    chore: install and config otl instrumentation

--- open-telemetry/instrumentation.ts
+++ open-telemetry/instrumentation.ts
@@ -5,8 +5,17 @@ import {
   ConsoleMetricExporter,
   PeriodicExportingMetricReader,
 } from "@opentelemetry/sdk-metrics";
+import { Resource } from "@opentelemetry/resources";
+import {
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_VERSION,
+} from "@opentelemetry/semantic-conventions";
 
 export const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "gamehub-api",
+    [SEMRESATTRS_SERVICE_VERSION]: "0.7.0",
+  }),
   traceExporter: new ConsoleSpanExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
