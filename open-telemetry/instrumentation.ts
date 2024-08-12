2f1ee4e72dabd8b6b9c534a18675ec7a5c9ca956
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-12 07:25:19 -0300

    feat: add opentelemetry initial config

---
+++ open-telemetry/instrumentation.ts
@@ -0,0 +1,15 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import {
+  ConsoleMetricExporter,
+  PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+
+export const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Strapi } from '@strapi/strapi';
+import { Strapi } from "@strapi/strapi";
+import { sdk } from "../open-telemetry/instrumentation";
 
 export default {
-
   register({ strapi }: { strapi: Strapi }) {
+    sdk.start();
   },
 
-  bootstrap(/*{ strapi }*/) { },
+  bootstrap(/*{ strapi }*/) {},
 };
